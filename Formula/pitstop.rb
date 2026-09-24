c34a7d82d107b5c9669c855385ff8450c249f51e
claide <magallen.fc@gmail.com>
2026-09-24 17:28:26 +0800

    pitstop 0.3.0

--- Formula/pitstop.rb
+++ Formula/pitstop.rb
@@ -1,8 +1,8 @@
 class Pitstop < Formula
   desc "Menu bar AI quota tracker with a quota check for ticket workflows"
   homepage "https://github.com/claide/pitstop"
-  url "https://github.com/claide/pitstop/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "52dd8c90516c495218f348881b948266df1245d4aa4ea8272849d80bbbe5cd24"
+  url "https://github.com/claide/pitstop/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "75fcc766882f799302f34be7f82792daac9edf41bbec53d68c20d8587e692c9b"
   license "MIT"
 
   depends_on macos: :sonoma
